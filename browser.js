c74c39c298c6f105379f407546423b1f40d6cf4e
Casper Beyer <caspervonb@qup.io>
2015-02-03 08:17:03 +0800

    Refactor exports

--- browser.js
+++ browser.js
@@ -1,60 +1,54 @@
 var events = require('events');
-var util = require('util');
 
-var window = global.window;
-var mantle = new events.EventEmitter();
-
-mantle.setRedraw = function redraw(callback) {
-  return window.requestAnimationFrame(callback);
+module.exports = new events.EventEmitter();
+module.exports.setRedraw = function setRedraw(callback) {
+  return global.requestAnimationFrame(callback);
 };
 
-mantle.clearRedraw = function redraw(redrawObject) {
-  return window.clearAnimationFrame(redrawObject);
+module.exports.clearRedraw = function clearRedraw(redrawObject) {
+  global.clearAnimationFrame(redrawObject);
 };
 
-['keyDown', 'keyUp'].forEach(function(event) {
-  window[fn = 'on' + event.toLowerCase()] = function(data) {
-    var key = data.key || data.which || data.keyCode;
-    var repeat = data.repeat;
-    var modifiers = undefined;
+(function register(emitter) {
+  ['keyDown', 'keyUp'].forEach(function(event) {
+    global[fn = 'on' + event.toLowerCase()] = function(data) {
+      var key = data.key || data.which || data.keyCode;
+      var repeat = data.repeat;
+      var modifiers = undefined;
 
-    if (mantle.emit(event, key, repeat, modifiers)) {
-      data.preventDefault();
-    }
-  };
-});
+      if (mantle.emit(event, key, repeat, modifiers)) {
+        data.preventDefault();
+      }
+    };
+  });
 
-['mouseDown', 'mouseUp', 'mouseMove'].forEach(function(event) {
-  window['on' + event.toLowerCase()] = function(data) {
-    var x = data.clientX;
-    var y = data.clientY;
-    var button = data.button;
-    var modifiers = undefined;
+  ['mouseDown', 'mouseUp', 'mouseMove'].forEach(function(event) {
+    global['on' + event.toLowerCase()] = function(data) {
+      var x = data.clientX;
+      var y = data.clientY;
+      var button = data.button;
+      var modifiers = undefined;
 
-    if (mantle.emit(event, x, y, button, modifiers)) {
-      data.preventDefault();
-    }
-  };
-});
+      if (mantle.emit(event, x, y, button, modifiers)) {
+        data.preventDefault();
+      }
+    };
+  });
 
-['touchStart', 'touchEnd', 'touchCancel', 'touchMove'].forEach(function(event) {
-  mantle[event] = function() {};
+  ['touchStart', 'touchEnd', 'touchCancel', 'touchMove'].forEach(function(event) {
+    global['on' + event.toLowerCase()] = function(data) {
+      var touches = data.touches;
+      if (mantle.emit(event, touches, modifiers)) {
+        data.preventDefault();
+      }
+    };
+  });
 
-  window['on' + event.toLowerCase()] = function(data) {
-    if (mantle.emit(event, modifiers)) {
-
-      data.preventDefault();
-    }
-  };
-});
-
-
-['focus', 'blur', 'resize'].forEach(function(event) {
-  window['on' + event.toLowerCase()] = function(data) {
-    if (mantle.emit(event)) {
-      data.preventDefault();
-    }
-  };
-});
-
-module.exports = mantle;
+  ['focus', 'blur', 'resize'].forEach(function(event) {
+    global['on' + event.toLowerCase()] = function(data) {
+      if (mantle.emit(event)) {
+        data.preventDefault();
+      }
+    };
+  });
+}());
