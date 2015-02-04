b9ada513ce0674015fb9772a93beec72b76af980
Casper Beyer <caspervonb@qup.io>
2015-02-04 09:42:49 +0800

    Refactor module exports

--- browser.js
+++ browser.js
@@ -1,17 +1,20 @@
 var events = require('events');
 
-module.exports = new events.EventEmitter();
-module.exports.setRedraw = function setRedraw(callback) {
-  return global.requestAnimationFrame(callback);
+var mantle = new events.EventEmitter();
+
+mantle.setRedraw = function (callback) {
+  global.requestAnimationFrame(callback);
 };
 
-module.exports.clearRedraw = function clearRedraw(redrawObject) {
-  global.clearAnimationFrame(redrawObject);
+mantle.clearRedraw = function (callback) {
+  global.cancelAnimationFrame(callback);
 };
 
-(function register(emitter) {
-  ['keyDown', 'keyUp'].forEach(function(event) {
-    global.addEventListener(event.toLowerCase(), function(data) {
+module.exports = mantle;
+
+global.addEventListener('load', function () {
+  ['keyDown', 'keyUp'].forEach(function (event) {
+    global.addEventListener(event.toLowerCase(), function (data) {
       var key = data.key || data.which || data.keyCode;
       var repeat = data.repeat;
       var modifiers = undefined;
@@ -19,11 +22,11 @@ module.exports.clearRedraw = function clearRedraw(redrawObject) {
       if (mantle.emit(event, key, repeat, modifiers)) {
         data.preventDefault();
       }
-    };
+    });
   });
 
-  ['mouseDown', 'mouseUp', 'mouseMove'].forEach(function(event) {
-    global.addEventListener(event.toLowerCase(), function(data) {
+  ['mouseDown', 'mouseUp', 'mouseMove'].forEach(function (event) {
+    global.addEventListener(event.toLowerCase(), function (data) {
       var x = data.clientX;
       var y = data.clientY;
       var button = data.button;
@@ -32,23 +35,23 @@ module.exports.clearRedraw = function clearRedraw(redrawObject) {
       if (mantle.emit(event, x, y, button, modifiers)) {
         data.preventDefault();
       }
-    };
+    });
   });
 
-  ['touchStart', 'touchEnd', 'touchCancel', 'touchMove'].forEach(function(event) {
-    global.addEventListener(event.toLowerCase(), function(data) {
+  ['touchStart', 'touchEnd', 'touchCancel', 'touchMove'].forEach(function (event) {
+    global.addEventListener(event.toLowerCase(), function (data) {
       var touches = data.touches;
       if (mantle.emit(event, touches, modifiers)) {
         data.preventDefault();
       }
-    };
+    });
   });
 
-  ['focus', 'blur', 'resize'].forEach(function(event) {
-    global.addEventListener(event.toLowerCase(), function(data) {
+  ['focus', 'blur', 'resize'].forEach(function (event) {
+    global.addEventListener(event.toLowerCase(), function (data) {
       if (mantle.emit(event)) {
         data.preventDefault();
       }
-    };
+    });
   });
-}());
+});
